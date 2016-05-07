e9e646c6338f7828faf68c49262b2bcf434b2b3e
saltire sable <saltiresable@gmail.com>
2016-05-07 01:01:21 -0400

    Move attack script from player to weapon

---
+++ Assets/WeaponAttackScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeaponAttackScript : MonoBehaviour {
+
+	public float weaponSpeed = 0.1f;
+	public float weaponDistance = 2f;
+
+	private bool isAttacking;
+	private bool isReturning;
+	private float weaponTime = 0f;
+
+	public void Attack () {
+		if (!isAttacking && !isReturning) {
+			isAttacking = true;
+			weaponTime = 0f;
+		}
+	}
+
+	void Update() {
+		if (isAttacking) {
+			weaponTime += Time.deltaTime;
+			transform.localPosition = Vector3.Lerp (
+				Vector3.zero, 
+				weaponDistance * Vector3.right, 
+				weaponTime / weaponSpeed);
+
+			if (weaponTime >= weaponSpeed) {
+				isAttacking = false;
+				isReturning = true;
+				weaponTime = 0f;
+			}
+		} else if (isReturning) {
+			weaponTime += Time.deltaTime;
+			transform.localPosition = Vector3.Lerp (
+				Vector3.zero, 
+				weaponDistance * Vector3.right, 
+				1 - weaponTime / weaponSpeed);
+
+			if (weaponTime >= weaponSpeed) {
+				isReturning = false;
+			}
+		}
+	}
+}
--- Assets/scripts/PlayerAttackScript.cs
+++ Assets/scripts/PlayerAttackScript.cs
@@ -4,58 +4,15 @@ using System.Collections;
 public class PlayerAttackScript : MonoBehaviour {
 
 	public int playerNumber = 1;
-	public float weaponSpeed = 0.25f;
-	public float weaponDistance = 0.25f;
-
-	private bool isAttacking = false;
-	private bool isReturning = false;
-	private float weaponTime = 0f;
-	private GameObject weapon;
-
-	void Start () {
-	}
 	
 	void Update () {
-		if (!isAttacking && !isReturning) {
-			float attack = Input.GetAxis ("Attack " + playerNumber);
-			if (attack != 0f) {
-				foreach (GameObject weaponObj in GameObject.FindGameObjectsWithTag("Weapon")) {
-					Debug.Log (gameObject);
-					Debug.Log (weaponObj);
-					if (weaponObj.transform.IsChildOf(gameObject.transform)) {
-						weapon = weaponObj;
-						isAttacking = true;
-						weaponTime = 0f;
-						break;
-					}
+		float attack = Input.GetAxis ("Attack " + playerNumber);
+		if (attack != 0f) {
+			foreach (GameObject weapon in GameObject.FindGameObjectsWithTag("Weapon")) {
+				if (weapon.transform.IsChildOf(gameObject.transform)) {
+					weapon.GetComponent<WeaponAttackScript>().Attack ();
+					break;
 				}
-			}
-		}
-
-		Vector3 facingDirection = gameObject.transform.rotation * Vector3.right;
-
-		if (isAttacking) {
-			weaponTime += Time.deltaTime;
-			weapon.transform.position = Vector3.Lerp (
-				gameObject.transform.position, 
-				gameObject.transform.position + weaponDistance * facingDirection, 
-				weaponTime / weaponSpeed);
-
-			if (weaponTime >= weaponSpeed) {
-				isAttacking = false;
-				isReturning = true;
-				weaponTime = 0f;
-			}
-		} 
-		else if (isReturning) {
-			weaponTime += Time.deltaTime;
-			weapon.transform.position = Vector3.Lerp (
-				gameObject.transform.position, 
-				gameObject.transform.position + weaponDistance * facingDirection, 
-				1 - weaponTime / weaponSpeed);
-
-			if (weaponTime >= weaponSpeed) {
-				isReturning = false;
 			}
 		}
 	}
